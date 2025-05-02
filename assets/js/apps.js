299df1988b452d66d2c0c4d10196b451049b01ef
VaibhavMojidra <vmojidra25@gmail.com>
2025-05-02 16:59:57 +0530

    Vaibhav Mojidra

---
+++ assets/js/apps.js
@@ -0,0 +1,216 @@
+let apps = [
+	{
+		appName: 'Count The Lines Of Code',
+		media: 'count_the_lines_of_code.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Count-The-Lines-Of-Code-Desktop-App-Using-Electron-JS',
+		project_type: 'Mini Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'count_the_lines_of_code.html',
+		tech: 'Electron JS'
+	},
+	{
+		appName: 'Aptitude Test with Answers',
+		media: 'aptitude_test.gif',
+		app_url: 'https://play.google.com/store/apps/details?id=com.vaibhavmojidra.aptitudetestapp',
+		project_type: 'Live Project',
+		button_text: 'Install App',
+		redirect_url: 'aptitude_test.html',
+		tech: 'React Native'
+	},
+	{
+		appName: 'PMATS',
+		media: 'pmats.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Popular-Movies-Artists-TV-Shows-Using-Clean-Architecture',
+		project_type: 'Mini Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'pmats.html',
+		tech: 'Android Kotlin'
+	},
+	{
+		appName: 'Daily News',
+		media: 'daily_news.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Daily-News-App-Using-React-Native',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'daily_news_app.html',
+		tech: 'React Native'
+	},
+	{
+		appName: 'Tracker App',
+		media: 'tracker_app.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Tracker-App-Using-React-Native-And-MongoDB',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'tracker_app.html',
+		tech: 'React Native'
+	},
+	{
+		appName: 'Student Profile',
+		media: 'studentprofile.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Student-Profile-Module-Using-React-Native',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'student_profile.html',
+		tech: 'React Native'
+	},
+	{
+		appName: 'Blog App',
+		media: 'blog.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Blog-App-Using-React-Native',
+		project_type: 'Mini Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'blog.html',
+		tech: 'React Native'
+	},
+	{
+		appName: 'Business Search',
+		media: 'business_search.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Business-Search-App-Using-React-Native-And-Yelp-API',
+		project_type: 'Mini Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'business_search.html',
+		tech: 'React Native'
+	},
+	{
+		appName: 'Hidden Launcher',
+		media: 'hidden_launcher.gif',
+		app_url: 'https://play.google.com/store/apps/details?id=com.vaibhavmojidra.hiddenlauncher',
+		project_type: 'Live Project',
+		button_text: 'Install App',
+		redirect_url: 'hidden_launcher.html',
+		tech: 'Android Java'
+	},
+	{
+		appName: 'Note Maker',
+		media: 'note_maker.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Note-Maker-Web-App-Using-Bootstrap-4-Hibernate-JSP-Servlet',
+		project_type: 'Mini Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'note_maker.html',
+		tech: 'Java Web'
+	},
+	{
+		appName: 'Doctor\'s Appointment',
+		media: 'doctors_app.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Doctor-s-Appointment-Application-Using-Firebase',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'doctors_app.html',
+		tech: 'Android Java'
+	},
+	{
+		appName: 'Programming eBooks',
+		media: 'programming_ebook.gif',
+		app_url: 'https://play.google.com/store/apps/details?id=com.vaibhavmojidra.programmingebooks',
+		project_type: 'Live Project',
+		button_text: 'Install App',
+		redirect_url: 'programming_ebooks.html',
+		tech: 'Android Kotlin'
+	}, {
+		appName: 'Shooting Birds',
+		media: 'shooting_birds.gif',
+		app_url: 'https://play.google.com/store/apps/details?id=com.vaibhavmojidra.shootingbirds',
+		project_type: 'Live Project',
+		button_text: 'Install App',
+		redirect_url: 'shooting_birds.html',
+		tech: 'Android Java'
+	},
+	{
+		appName: 'Programming MCQ App',
+		media: 'programming_mcq.gif',
+		app_url: 'https://play.google.com/store/apps/details?id=com.vaibhavmojidra.programmingmcqapp',
+		project_type: 'Live Project',
+		button_text: 'Install App',
+		redirect_url: 'programming_mcq_app.html',
+		tech: 'Android Kotlin'
+	},
+	{
+		appName: 'College Management System',
+		media: 'notify_me.gif',
+		app_url: 'pages/preview/notify_me_doc.html',
+		project_type: 'Demo Project',
+		button_text: 'Preview Link',
+		redirect_url: 'notify_me.html',
+		tech: 'Android Java'
+	},
+	{
+		appName: "Where's the food",
+		media: 'wheres_the_food.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Wheres-the-food-Using-Android-Java',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'wheres_the_food.html',
+		tech: 'Android Java'
+	},
+	{
+		appName: 'Quick Chat',
+		media: 'quick_chat.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Android-Based-Chat-Application-Using-Java-And-Firebase',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'quick_chat.html',
+		tech: 'Android Java'
+	},
+	{
+		appName: 'Database Query Executor',
+		media: 'data_query_executor.gif',
+		app_url: 'https://github.com/VaibhavMojidra/MS-SQL-Database-Query-Executer-Using-Java',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'data_query_executor.html',
+		tech: 'Java Desktop'
+	},
+	{
+		appName: 'Java MCQ Web App',
+		media: 'java_mcq.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Java-MCQ-Web-Application',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'java_mcq.html',
+		tech: 'Java Web'
+	},
+	{
+		appName: 'CAPP',
+		media: 'capp.gif',
+		app_url: 'https://play.google.com/store/apps/details?id=com.cordova.vaibhavcapp',
+		project_type: 'Live Project',
+		button_text: 'Install App',
+		redirect_url: 'capp.html',
+		tech: 'Apache Cordova'
+	},
+	{
+		appName: 'Password Based Circuit Breaker',
+		media: 'password_based_circuit_breaker.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Password-Based-Circuit-Breaker-Using-Arduino-UNO',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'password_based_circuit_breaker.html',
+		tech: 'Embedded System'
+	},
+	{
+		appName: 'VMOS',
+		media: 'vmos.gif',
+		app_url: 'https://github.com/VaibhavMojidra/Virtual-Machine-Operating-System-VMOS-Using-Java',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'vmos.html',
+		tech: 'Java Desktop'
+	}, {
+		appName: 'ATM',
+		media: 'atm.gif',
+		app_url: 'https://github.com/VaibhavMojidra/ATM-Desktop-Application-Using-JAVA',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'atm.html',
+		tech: 'Java Desktop'
+	},
+	{
+		appName: 'CAPP Computer Version',
+		media: 'capp_com.gif',
+		app_url: 'https://github.com/VaibhavMojidra/CAPP-Computer-Version',
+		project_type: 'Demo Project',
+		button_text: 'GitHub Repository',
+		redirect_url: 'capp_com.html',
+		tech: 'Java Desktop'
+	}
+];
---
+++ assets/js/work.js
@@ -0,0 +1,46 @@
+let work = [
+    {
+        companyName: "Deloitte USI (US Office of India)",
+        companyLogo: "deloitte.jpeg",
+        role: '',
+        companyOfficialWebsite: 'https://www2.deloitte.com/ui/en.html',
+        companyJoiningDate: '1/18/2021',
+        companyLeavingDate: 'Present',
+        projects: [
+            {
+                client: "AARP",
+                clientLogo: "aarp.jpg",
+                clientWebsite: 'https://www.aarp.org/',
+                projectRoleDesc: 'Android Developer',
+                projectJoiningDate: '1/2/2024',
+                projectEndDate: 'Present'
+            },
+            {
+                client: "Google LLC",
+                clientLogo: "google.jpeg",
+                clientWebsite: 'https://about.google/',
+                projectRoleDesc: 'SAP UI5 Developer',
+                projectJoiningDate: '3/30/2021',
+                projectEndDate: '12/31/2023'
+            }
+        ]
+    }
+]
+
+let awards = [
+    {
+        awardname: 'Applause Award',
+        awardDate: '04/07/2023',
+        awardNote: 'Received from Deloitte USI while working for Google'
+    },
+    {
+        awardname: 'Applause Award',
+        awardDate: '07/26/2022',
+        awardNote: 'Received from Deloitte USI while working for Google'
+    },
+    {
+        awardname: 'Spot Award',
+        awardDate: '01/30/2022',
+        awardNote: 'Received from Deloitte USI while working for Google'
+    }
+]
